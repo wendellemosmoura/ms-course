13259c129f7fa78528100e841434b145cefce75c
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2023-06-16 15:33:21 -0300

    Project hr-worker created

---
+++ hrworker/src/test/java/com/wendellemos/hrworker/HrworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendellemos.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
