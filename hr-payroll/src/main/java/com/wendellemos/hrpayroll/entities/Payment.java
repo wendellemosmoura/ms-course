31daf21af656673b2b64ffa07226a8e8102e6078
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2023-06-17 14:51:20 -0300

    Feign client

--- hr-payroll/src/main/java/com/wendellemos/hrpayroll/entities/Payment.java
+++ hr-payroll/src/main/java/com/wendellemos/hrpayroll/entities/Payment.java
@@ -1,46 +1,19 @@
 package com.wendellemos.hrpayroll.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class Payment implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private String name;
     private Double dailyIncome;
     private Integer days;
-
-    public Payment() {
-    }
-
-    public Payment(String name, Double dailyIncome, Integer days) {
-        this.name = name;
-        this.dailyIncome = dailyIncome;
-        this.days = days;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getDailyIncome() {
-        return dailyIncome;
-    }
-
-    public void setDailyIncome(Double dailyIncome) {
-        this.dailyIncome = dailyIncome;
-    }
-
-    public Integer getDays() {
-        return days;
-    }
-
-    public void setDays(Integer days) {
-        this.days = days;
-    }
 
     public double getTotal() {
         return days * dailyIncome;
