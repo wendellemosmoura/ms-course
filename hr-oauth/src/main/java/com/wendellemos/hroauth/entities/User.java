5b9d343f2a370cfb7a09c6b2f5dd7ea7daace0a3
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2023-06-21 10:54:55 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/wendellemos/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/wendellemos/hroauth/entities/User.java
@@ -1,13 +1,18 @@
 package com.wendellemos.hroauth.entities;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
@@ -17,4 +22,34 @@ public class User implements Serializable {
     private String password;
 
     Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
